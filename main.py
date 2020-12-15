7b5bb34621db5913545081c04d7062fac3959072
Gaetan Jagorel <gaetan.jagorel@etu.univ-tours.fr>
2020-12-15 15:25:15 +0100

    model training, bug convert Numpy array to a tensor

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ import os
 
 def training():
     folder = "./Tagging/"
+    classification = ["yellow", "green", "red", "blue", "brown", "gray", "white", "black", "orange",
+                      "inside", "outside", "nature", "city",
+                      "human", "face", "animal", "car", "plane", "van", "byke",
+                      "sky", "sun", "house", "tree", "water", "sea", "mountain", "snow", 'ski',
+                      "fuzzy", "sharp", "dark", "light",
+                      "solated", "multiple"]
 
     x_set = []
     y_set = []
@@ -20,12 +26,43 @@ def training():
     for file in os.listdir(folder):
         if file != "GT":
             x_set.append(cv2.imread(folder + file))
+
             labels = open(folder + "GT/" + file.split('.')[0] + ".txt").read()
-            y_set.append(labels)
+            labels = labels.split(',')
+            for i in range(len(labels)):
+                labels[i] = classification.index(labels[i].replace(" ", ""))
 
-    print(x_set)
-    print(y_set)
+            y_set.append(labels[0])
 
+    x_train = np.array(x_set)
+
+
+    y_train = np.array(y_set)
+    y_train_one_hot = to_categorical(y_train)
+
+    x_train = x_train/255
+
+    model = Sequential()
+    model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(32, 32, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(1000, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(500, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(250, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+
+
+    hist = model.fit(x_train, y_train_one_hot,
+                     batch_size=256, epochs=10, validation_split=0.2)
 
 
 if __name__ == '__main__':
